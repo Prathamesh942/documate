42a6dbbbd38b34f7eb94816d4ff95ab4a380264c
prathamesh <prathameshsk94@gmail.com>
2024-08-27 15:35:52 +0530

    fix clear cookies

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,6 +27,18 @@ const Home = () => {
     getDocs();
   }, []);
 
+  const clearCookies = () => {
+    const cookies = document.cookie.split(";");
+
+    cookies.forEach((cookie) => {
+      const eqPos = cookie.indexOf("=");
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;`;
+    });
+
+    console.log("All cookies have been cleared.");
+  };
+
   const navigate = useNavigate();
 
   return (
